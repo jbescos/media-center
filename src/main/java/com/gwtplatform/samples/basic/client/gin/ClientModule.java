87f091238291e09ab210e37ba834f8e59057c5c4
jbescos <jorgebescos@gmail.com>
2023-12-28 14:19:51 +0100

    Moving to GWTP
    Signed-off-by: jbescos <jorgebescos@gmail.com>

---
+++ src/main/java/com/gwtplatform/samples/basic/client/gin/ClientModule.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.basic.client.gin;
+
+import com.gwtplatform.dispatch.rpc.client.gin.RpcDispatchAsyncModule;
+import com.gwtplatform.mvp.client.annotations.GaAccount;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+import com.gwtplatform.mvp.client.gin.DefaultModule;
+import com.gwtplatform.samples.basic.client.application.ApplicationModule;
+import com.gwtplatform.samples.basic.client.place.NameTokens;
+import com.gwtplatform.samples.basic.client.resources.ResourceLoader;
+
+public class ClientModule extends AbstractPresenterModule {
+    private static final String ANALYTICS_ACCOUNT = "UA-8319339-6";
+
+    @Override
+    protected void configure() {
+        install(new DefaultModule.Builder()
+                .defaultPlace(NameTokens.home)
+                .errorPlace(NameTokens.home)
+                .unauthorizedPlace(NameTokens.home)
+                .build());
+        install(new RpcDispatchAsyncModule());
+        install(new ApplicationModule());
+
+        bindConstant().annotatedWith(GaAccount.class).to(ANALYTICS_ACCOUNT);
+
+        bind(ResourceLoader.class).asEagerSingleton();
+    }
+}
